5e1847ec467c0f48e65a289e34e7331267b76003
Manuel Félix <manuel@manuelfelix.eu>
2022-01-14 16:59:30 +0000

    commit on main

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { res } = require('express')
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const mongoose = require ('mongoose')
+const Person = require('./models/people')
+const { response } = require('express')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -11,41 +14,6 @@ app.use(cors())
 morgan.token('body', (req,res) => {return JSON.stringify(req.body)} )
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms {:body}'))
-
-// const reqLogger = (req, res, next) => {
-//   console.log('Method:', req.method)
-//   console.log('Path:  ', req.path)
-//   console.log('Body:  ', req.body)
-//   console.log('---')
-//   next()
-// }
-
-// app.use(reqLogger)
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const text = `Phonebook has info for ${persons.length} people`
 
 app.get('/', (req, res) => {
     res.send(
@@ -57,47 +25,59 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
     res.json(persons)
+  })
+    
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person){
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person){
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
-  if(!body.name){
-    return res.status(400).json({
-      error: 'content missing'
-    })
-  } else if(persons.map(n=>n.name).includes(body.name)){
-    return res.status(400).json({
-      error: 'name must be unique'
-    })
-  }
 
-  const person = {
-    id: Math.floor(Math.random() * 10000),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons = persons.concat(person)
-
-  res.json(person)
+  person.save()
+    .then(savePerson => {
+      res.json(savePerson)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (req,res,next) => {
+  const body = req.body
+  const person = {
+    number: body.number
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new:true })
+    .then(updatePerson => {
+      res.json(updatePerson)
+    })
+    .catch(error => next(error))
+})
 
-  res.status(204).end()
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
@@ -106,7 +86,22 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message})
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`)
 })
